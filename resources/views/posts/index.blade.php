9f9c6057080f934b9a3a7ae8b79c18eb42587c47
initialgr <initial.gr.101@gmail.com>
2024-02-28 21:55:56 +0700

    Menampilkan Array Statis

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,10 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog</title>
+    <style>
+        .blog {
+            padding: 5px;
+            border-bottom: 1px solid lightgray;
+        }
+
+        small {
+            color: gray;
+        }
+    </style>
 </head>
 
 <body>
     <h1>Blog Codepolitan</h1>
+    <div>
+        @php($number = 1)
+        @foreach ($posts as $post)
+            <div class="blog">
+
+                <h3>{{ $post[0] }} <small>#{{ $number }}</small></h3>
+                <p>{{ $post[1] }}</p>
+            </div>
+            @php($number++)
+        @endforeach
+    </div>
 </body>
 
 </html>
