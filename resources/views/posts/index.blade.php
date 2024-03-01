f4ea124629f9adadec36c5d400bccc4b6610351b
initialgr <initial.gr.101@gmail.com>
2024-03-01 16:44:06 +0700

    Install Bootstrap

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog</title>
+    <!-- CSS -->
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Javascript -->
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <style>
         .blog {
             padding: 5px;
@@ -19,16 +24,16 @@
 </head>
 
 <body>
-    <h1>Blog Codepolitan</h1>
-    <div>
+    <div class="container">
+        <h1>Blog Codepolitan</h1>
         @php($number = 1)
         @foreach ($posts as $post)
-            <div class="blog">
+        <div class="blog">
 
-                <h3>{{ $post[0] }} <small>#{{ $number }}</small></h3>
-                <p>{{ $post[1] }}</p>
-            </div>
-            @php($number++)
+            <h3>{{ $post[0] }} <small>#{{ $number }}</small></h3>
+            <p>{{ $post[1] }}</p>
+        </div>
+        @php($number++)
         @endforeach
     </div>
 </body>
