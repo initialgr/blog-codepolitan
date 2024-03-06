f3fc2fbd8f8442c23a35b825011f1794b397a100
initialgr <initial.gr.101@gmail.com>
2024-03-06 19:12:04 +0700

    Membuat Form Tambah Data

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog | Buat Postingan</title>
+    <!-- CSS -->
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Javascript -->
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <h1>Buat Postingan</h1>
+    <form action="POST" action="{{ url('posts') }}" class="form-control">
+        <div class="mb-3">
+            <label for="title" class="form-label">Judul</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+</body>
+
+</html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,9 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog</title>
     <!-- CSS -->
-    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Javascript -->
-    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <style>
         .blog {
@@ -25,18 +27,21 @@
 
 <body>
     <div class="container">
-        <h1>Blog Codepolitan</h1>
+        <h1>Blog Codepolitan
+            <a href="{{ url('posts/create') }}" class="btn btn-success">+ Buat Postingan</a>
+        </h1>
 
         @foreach ($posts as $post)
-        @php($post = explode(",", $post))
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">{{ $post[1] }}</h5>
-                <p class="card-text">{{ $post[2] }}</p>
-                <p class="card-text"><small class="text-muted">Last updated at {{ date('d M Y H:i', strtotime($post[3])) }}</small></p>
-                <a href="{{ url("posts/$post[0]") }}" class="btn btn-primary">Selengkapnya</a>
+            @php($post = explode(',', $post))
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post[1] }}</h5>
+                    <p class="card-text">{{ $post[2] }}</p>
+                    <p class="card-text"><small class="text-muted">Last updated at
+                            {{ date('d M Y H:i', strtotime($post[3])) }}</small></p>
+                    <a href="{{ url("posts/$post[0]") }}" class="btn btn-primary">Selengkapnya</a>
+                </div>
             </div>
-        </div>
         @endforeach
     </div>
 </body>
