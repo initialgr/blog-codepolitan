f368c9619f694cde7bfb03a8e30dbe363cb6589c
initialgr <initial.gr.101@gmail.com>
2024-03-06 19:57:24 +0700

    menambahkan data kedalam file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,7 +40,25 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $title = $request->input('title');
+        $content = $request->input('content');
+        $posts = Storage::get('posts.txt');
+        $posts = explode("\n", $posts);
+
+        $new_post = [
+            count($posts) + 1,
+            $title,
+            $content,
+            date('Y-m-d H:i:s')
+        ];
+        $new_post = implode(',', $new_post);
+
+        array_push($posts, $new_post);
+        $posts = implode("\n", $posts);
+
+        Storage::write('posts.txt', $posts);
+
+        return redirect('posts');
     }
 
     /**
