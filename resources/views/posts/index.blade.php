263c21011d471bf6efc498b93142350a14deaa77
initialgr <initial.gr.101@gmail.com>
2024-03-08 13:59:32 +0700

    Membuat Header _ Footer

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,6 +26,19 @@
 </head>
 
 <body>
+    <header class="p-3 text-bg-dark mb-3">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                </ul>
+
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-light me-2">Login</button>
+                </div>
+            </div>
+        </div>
+    </header>
     <div class="container">
         <h1>Blog Codepolitan
             <a href="{{ url('posts/create') }}" class="btn btn-success">+ Buat Postingan</a>
@@ -43,7 +56,16 @@
                 </div>
             </div>
         @endforeach
+
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            </ul>
+        </footer>
     </div>
+
 </body>
 
 </html>
