2e47e84ba19bac2ac9ff727f4aec23e3684b28a5
initialgr <initial.gr.101@gmail.com>
2024-03-08 18:24:57 +0700

    Proses Otentikasi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,8 +17,13 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function authenticate()
+    public function authenticate(Request $request)
     {
-        //
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect('posts');
+        } else {
+            return redirect('login')->with('error_message', 'Wrong email or password');
+        }
     }
 }
