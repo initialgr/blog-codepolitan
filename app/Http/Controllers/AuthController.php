6969c4848e5b7d89cde92b5445403a36480b11dd
initialgr <initial.gr.101@gmail.com>
2024-03-09 13:55:26 +0700

    Menambahkan Form Registrasi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,4 +35,14 @@ class AuthController extends Controller
 
         return redirect('login');
     }
+
+    public function register_form()
+    {
+        return view('auth.register');
+    }
+
+    public function register()
+    {
+        //
+    }
 }
